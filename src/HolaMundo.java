b610a4b58ecc1698e6dbbd9e9d769589c961e4ff
Tardes <Tardes@GET-01>
2024-11-26 18:01:19 +0100

    Nueva modificación del dato.

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,7 +2,7 @@
 public class HolaMundo {
 	public static void main (String[] args) {
 		//Comentarios de línea
-		System.out.println("Hola Mundo2");
+		System.out.println("Hola Mundo");
 		/*
 		 * Comentario
 		 * de
