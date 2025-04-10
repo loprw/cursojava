acf61f5d8c16254a50250bf8980bda5e757e71b1
loprw <loprwdemileto@gmail.com>
2025-04-10 20:41:02 +0200

    Clase 10/04/2025
    Se añade/modifica:
    
    - Se crea la clase EstudianteDAOImpl.java en .es.cursojava.dao.
    - Se modifica la clase MainBiblioteca.java en
    .hibernate.ejercicios.biblioteca.
    - Se modifica la clase BibliotecaService.java en .es.cursojava.services.
    - Se crea la clase EstudianteDTO.java en .es.cursojava.dto.
    - Se crea el paquete front en .es.cursojava.
    - se crea la clase BibliotecaFront.java en .es.cursojava.front.

--- src/es/cursojava/hibernate/ejercicios/biblioteca/MainBiblioteca.java
+++ src/es/cursojava/hibernate/ejercicios/biblioteca/MainBiblioteca.java
@@ -1,72 +1,12 @@
 package es.cursojava.hibernate.ejercicios.biblioteca;
 
-import java.util.Date;
-import java.util.List;
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import es.cursojava.entities.CarnetBiblioteca;
-import es.cursojava.entities.Estudiante;
-import es.cursojava.hibernate.HibernateUtil;
-import es.cursojava.utils.Utilidades;
+import es.cursojava.front.BibliotecaFront;
 
 public class MainBiblioteca {
-	
-    private static Session session = HibernateUtil.getSession();
 
 	public static void main(String[] args) {
 	
-		agregarEstudiante();
-		mostrarEstudiantes();
-	}
-	
-	//DAO
-	private static boolean agregarEstudiante() {
-		
-		boolean resultado = false;
-		
-		Estudiante estudiante = crearEstudiante();
-		
-		Transaction tx = session.beginTransaction();
-		
-		session.persist(estudiante);
-		
-		tx.commit();
-		
-		if (estudiante != null) {
-			resultado = true;
-		}
-		
-		return resultado;
-	}
-
-	//servicio
-	private static Estudiante crearEstudiante() {
-		
-		String nombre = Utilidades.pideDatoCadena("Indica el nombre del estudiante:");
-		String apellidos = Utilidades.pideDatoCadena("Indica los apellidos del estudiante");
-		CarnetBiblioteca carnet = crearCarnet();
-		
-		return new Estudiante(nombre, apellidos, carnet);
-	}
-	
-	//servicio
-	private static CarnetBiblioteca crearCarnet() {
-				
-		Date fecha = Utilidades.convertirFecha();
-		
-		return new CarnetBiblioteca(fecha);
-	}
-	
-	//DAO
-	private static void mostrarEstudiantes() {
-		
-		String query = "from Estudiante";
-		List<Estudiante> listado = session.createQuery(query, Estudiante.class).list();
-		
-		for (Estudiante estudiante : listado) {
-			System.out.println(estudiante);
-		}
+		BibliotecaFront front = new BibliotecaFront();
+		front.mostrarMenu();
 	}
 }
--- src/es/cursojava/services/BibliotecaService.java
+++ src/es/cursojava/services/BibliotecaService.java
@@ -1,5 +1,43 @@
 package es.cursojava.services;
 
-public class BibliotecaService {
+import java.util.Date;
+import java.util.List;
 
+import es.cursojava.dao.EstudianteDAOImpl;
+import es.cursojava.dto.EstudianteDTO;
+import es.cursojava.entities.CarnetBiblioteca;
+import es.cursojava.entities.Estudiante;
+import es.cursojava.utils.Utilidades;
+
+public class BibliotecaService {
+	
+	EstudianteDAOImpl est = new EstudianteDAOImpl();
+	
+	public void crearEstudiante(EstudianteDTO estudiante) {
+		
+		String nombre = estudiante.getNombre();
+		String apellidos = estudiante.getApellidos();
+		CarnetBiblioteca carnet = crearCarnet(estudiante.getFechaEmision());
+		
+		
+		est.agregarEstudiante(new Estudiante(nombre, apellidos, carnet));
+	}
+	
+	private CarnetBiblioteca crearCarnet() {
+		
+		Date fecha = Utilidades.convertirFecha();
+		
+		return new CarnetBiblioteca(fecha);
+	}
+	
+	private CarnetBiblioteca crearCarnet(Date fecha) {
+		return new CarnetBiblioteca(fecha);
+	}
+	
+	public List<Estudiante> mostrarEstudiantesService() {
+		
+		List<Estudiante> listado = est.mostrarEstudiantes();
+		
+		return listado;
+	}
 }
